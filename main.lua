717e19bc3c2e6ad63651240b6b1d47030812eeec
Jonathan Payne <jonathanjpayne@gmail.com>
2015-03-11 20:40:31 -0700

    slightly better map stuff

--- main.lua
+++ main.lua
@@ -44,7 +44,7 @@ function love.load()
 	--
 	map = sti.new("data/maps/00")
 	gal1 = Player()
-	gal1:setPosition(SCREEN_CENTER_X,SCREEN_CENTER_Y)
+	gal1:setPosition(SCREEN_CENTER_X - 128,SCREEN_CENTER_Y)
 	
 	addVelocity = vector(0,0)
 	
@@ -75,7 +75,7 @@ end
 function love.update(dt)
 	game.time = game.time + dt
 	
-	addVelocity.y = addVelocity.y + 9.8
+	addVelocity.y = addVelocity.y + 0.125
 	
 	gal1:move(addVelocity * dt)
 	
@@ -88,8 +88,12 @@ function love.update(dt)
 			local _x = math.ceil( x / 32 )
 			local _y = math.ceil( y / 32 )
 			
-			if map.layers["Tile Layer 1"].data[_y][_x] ~= nil then
-				numTiles[#numTiles+1] = map.layers["Tile Layer 1"].data[_x][_y]
+			if map.layers["Tile Layer 1"].data[_y][_x] then
+				if numTiles then
+					numTiles[#numTiles+1] = map.layers["Tile Layer 1"].data[_x][_y]
+					numTiles[#numTiles].x = _x * 32
+					numTiles[#numTiles].y = _y * 32
+				end
 			end
 			x = x + 32
 			y = y + 32
@@ -97,8 +101,8 @@ function love.update(dt)
 	end
 	
 	--
-	for i,t in ipairs(numTiles) do
-		print( t.offset.x )
+	for i, t in ipairs(numTiles) do
+		print( t.x + t.offset.x .. "," .. t.y + t.offset.y )
 	end
 end
 
@@ -119,4 +123,13 @@ function love.draw()
 	love.graphics.print( love.timer.getFPS(), 16, 12 )
 	love.graphics.print( #map.layers["Tile Layer 1"].data, 16, 12 + 16)
 	love.graphics.print( #numTiles, 16, 12 + 16*2)
+	
+	love.graphics.push()
+	for i, t in ipairs(numTiles) do
+		love.graphics.setColor(255,0,0)
+		love.graphics.rectangle("line", t.x + t.offset.x, t.y + t.offset.y, t.width, t.height )
+		love.graphics.setColor(255,0,255)
+		love.graphics.print( t.x .. "," .. t.y, t.x + t.offset.x, t.y + t.offset.y )
+	end
+	love.graphics.pop()
 end
