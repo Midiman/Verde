6775701705e0576ffc0bd492303fb88b4a8965fb
Jonathan Payne <jonathanjpayne@gmail.com>
2015-03-12 04:45:37 -0700

    collisions respecting the map now

--- main.lua
+++ main.lua
@@ -44,8 +44,9 @@ function love.load()
 	--
 	map = sti.new("data/maps/00")
 	gal1 = Player()
-	gal1:setPosition(SCREEN_CENTER_X - 128,SCREEN_CENTER_Y)
-	
+	gal1:setMap( map )
+	gal1:setPosition(480,SCREEN_CENTER_Y)
+
 	addVelocity = vector(0,0)
 	
 	numTiles = {}
@@ -74,36 +75,7 @@ end
 
 function love.update(dt)
 	game.time = game.time + dt
-	
-	addVelocity.y = addVelocity.y + 0.125
-	
-	gal1:move(addVelocity * dt)
-	
-	--
-	local prevx, prevy = gal1:getPosition()
-	local x, y = prevx - 64, prevy - 64
-	numTiles = {}
-	while x < prevx + 64 do
-		while y < prevy + 64 do
-			local _x = math.ceil( x / 32 )
-			local _y = math.ceil( y / 32 )
-			
-			if map.layers["Tile Layer 1"].data[_y][_x] then
-				if numTiles then
-					numTiles[#numTiles+1] = map.layers["Tile Layer 1"].data[_x][_y]
-					numTiles[#numTiles].x = _x * 32
-					numTiles[#numTiles].y = _y * 32
-				end
-			end
-			x = x + 32
-			y = y + 32
-		end
-	end
-	
-	--
-	for i, t in ipairs(numTiles) do
-		print( t.x + t.offset.x .. "," .. t.y + t.offset.y )
-	end
+	gal1:update(dt)
 end
 
 function love.focus(f)
@@ -121,15 +93,5 @@ function love.draw()
 	love.graphics.rectangle("fill", 16,12,256,32)
 	love.graphics.setColor(255,255,255)
 	love.graphics.print( love.timer.getFPS(), 16, 12 )
-	love.graphics.print( #map.layers["Tile Layer 1"].data, 16, 12 + 16)
-	love.graphics.print( #numTiles, 16, 12 + 16*2)
-	
-	love.graphics.push()
-	for i, t in ipairs(numTiles) do
-		love.graphics.setColor(255,0,0)
-		love.graphics.rectangle("line", t.x + t.offset.x, t.y + t.offset.y, t.width, t.height )
-		love.graphics.setColor(255,0,255)
-		love.graphics.print( t.x .. "," .. t.y, t.x + t.offset.x, t.y + t.offset.y )
-	end
-	love.graphics.pop()
+	love.graphics.print( string.format("%0.2f,%0.2f", gal1:getVelocity() ), 16, 12 + 16)
 end
--- player.lua
+++ player.lua
@@ -9,13 +9,20 @@ Player = Class {
 	init = function(self, x, y)
 		self.position = Vector(x,y)
 		self.velocity = Vector(0,0)
+		self.grounded = false
 		self.bounds = Vector(32,32)
+		self.map = nil
+		self.lastTile = Vector(0,0)
+		self.color = {255,255,255}
 	end
 }
+function Player:setMap( map )
+	self.map = map
+end
 function Player:setPosition(x, y)
 	self.position = Vector(x,y)
 end
-
+function Player:isGrounded() return self.grounded end
 function Player:move(v)
 	self.position = self.position + v
 end
@@ -24,17 +31,67 @@ function Player:getPosition()
 	return self.position:unpack()
 end
 
+function Player:getVelocity()
+	return self.velocity:unpack()
+end
 function Player:onCollision(entity, dx, dy)
 
 end
 
+function Player:update(dt)
+	if not self.grounded then
+		self.velocity.y = self.velocity.y + GRAVITY
+	end
+	self:move( self.velocity * dt)
+	self.velocity.x = 0
+	local x, y = self.position:unpack()
+	local tx, ty = self.map:convertScreenToTile(x, y)
+	
+	tx = math.floor(tx)
+	ty = math.floor(ty)
+	self.lastTile = Vector(tx,ty)
+	
+	self.grounded = false
+	if self.map.layers["Tile Layer 1"].data[ty+1][tx+1] then
+		local dy = (ty * self.map.tileheight) - self.position.y
+		local dx = (tx * self.map.tilewidth) - self.position.x
+
+		self.grounded = true
+		self.position.y = self.position.y + dy
+		self.velocity.y = 0 
+	end
+	self.color = self.grounded and {255,255,0} or {0,255,255}
+	
+	if self.grounded and love.keyboard.isDown("up") then
+		self.velocity.y = -GRAVITY * self.map.tileheight
+	end
+	if love.keyboard.isDown("left") then
+		self.velocity.x = -128
+	end
+	if love.keyboard.isDown("right") then
+		self.velocity.x = 128
+	end
+end
+
 function Player:draw()
 	love.graphics.push()
-	love.graphics.setColor(255,192,192)
+	love.graphics.setColor( unpack(self.color) )
 	love.graphics.rectangle("fill",
-		self.position.x - self.bounds.x/2, self.position.y - self.bounds.y/2,
+		self.position.x, self.position.y,
 		self.bounds.x, self.bounds.y
 	)
+	love.graphics.setColor(0,255,255)
+	love.graphics.rectangle("line",
+		self.lastTile.x * self.map.tilewidth, self.lastTile.y * self.map.tileheight,
+		self.map.tilewidth,self.map.tileheight
+	)
+	love.graphics.setColor(255,0,0)
+	love.graphics.rectangle("fill",
+		self.lastTile.x * self.map.tilewidth, (math.ceil( (self.position.y + 1 ) / self.map.tileheight) * self.map.tileheight),
+		self.map.tileheight, 2
+	)
+	love.graphics.setColor(255,0,255)
+	love.graphics.circle("fill", self.position.x, self.position.y, 4, 4)
 	love.graphics.pop()
 end
 
